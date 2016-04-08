ed97a8797ce9e9145f3088851ce5d1487dce9fa3
madhu <madsnapdragon@gmail.com>
2016-04-08 21:37:15 +0200

    gaussianKernel without printing x1, x2

--- machine-learning-ex6/ex6/gaussianKernel.m
+++ machine-learning-ex6/ex6/gaussianKernel.m
@@ -6,8 +6,6 @@ function sim = gaussianKernel(x1, x2, sigma)
 % Ensure that x1 and x2 are column vectors
 x1 = x1(:); x2 = x2(:);
 
-x1
-x2
 % You need to return the following variables correctly.
 sim = 0;
 
