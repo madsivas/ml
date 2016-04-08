be9ebc183bdc284e1551628c3964c5306a50d3d0
madhu <madsnapdragon@gmail.com>
2016-04-08 21:33:52 +0200

    w-i-p dataset3Params

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -24,6 +24,19 @@ sigma = 0.3;
 %
 
 
+min_pred_error = 100000000;
+for c = [0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30]
+   for s = [0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30]
+      model= svmTrain(X, y, c, @(x1, x2) gaussianKernel(x1, x2, s)); 
+      predictions = svmPredict(model, Xval);
+      pred_error = mean(double(predictions ~= yval));
+      if pred_error < min_pred_error
+         min_pred_error = pred_error;
+         C = c;
+         sigma = s;
+      endif
+   endfor
+endfor
 
 
 
